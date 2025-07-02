ca203bca29db808edc5759df15ab6a20473ab804
Viktor Lidholt <viktorlidholt@gmail.com>
2025-07-02 12:33:42 +0200

    feat: Initial work on a data source for GitHub discussions.

--- starguide_server/lib/src/business/data_sources/github_discussions.dart
+++ starguide_server/lib/src/business/data_sources/github_discussions.dart
@@ -1,86 +1,115 @@
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-// import 'package:starguide_server/src/business/data_sources/github_docs.dart';
+import 'dart:convert';
 
-// Future<void> fetchAnsweredGithubDiscussions({
-//   required String owner,
-//   required String repo,
-//   required String categoryId,
-// }) async {
-//   final query = '''
-//     query {
-//       repository(owner: "$owner", name: "$repo") {
-//         discussions(first: 50, categoryId: "$categoryId") {
-//           nodes {
-//             title
-//             url
-//             answerChosenAt
-//             body
-//             answer {
-//               body
-//               createdAt
-//             }
-//           }
-//         }
-//       }
-//     }
-//   ''';
+import 'package:http/http.dart' as http;
+import 'package:serverpod/serverpod.dart';
+import 'package:starguide_server/src/business/data_fetcher.dart';
+import 'package:starguide_server/src/business/data_source.dart';
 
-//   final response = await http.post(
-//     Uri.parse('https://api.github.com/graphql'),
-//     headers: {
-//       'Authorization': 'Bearer $githubToken',
-//       'Content-Type': 'application/json',
-//     },
-//     body: jsonEncode({'query': query}),
-//   );
+class GithubDiscussionsDataSource implements DataSource {
+  final String owner;
+  final String repo;
+  final String categoryName;
 
-//   final data = jsonDecode(response.body);
-//   final discussions = data['data']['repository']['discussions']['nodes'];
+  GithubDiscussionsDataSource({
+    required this.owner,
+    required this.repo,
+    required this.categoryName,
+  });
 
-//   for (var discussion in discussions) {
-//     if (discussion['answerChosenAt'] != null) {
-//       print('${discussion['title']}: ${discussion['url']}');
-//       print(discussion['body']);
-//     }
-//   }
-// }
+  @override
+  String get name => 'GithubDiscussions';
 
-// Future<String?> fetchGithubDiscussionCategoryId({
-//   required String owner,
-//   required String repo,
-//   required String name,
-// }) async {
-//   final query = '''
-//     query {
-//       repository(owner: "$owner", name: "$repo") {
-//         discussionCategories(first: 50) {
-//           nodes {
-//             id
-//             name
-//           }
-//         }
-//       }
-//     }
-//   ''';
+  @override
+  Stream<RawRAGDocuement> fetch(
+    Session session,
+    DataFetcher fetcher, {
+    String? path,
+    Uri? referenceUrl,
+  }) async* {
+    final githubToken = Serverpod.instance.getPassword('githubToken')!;
 
-//   final response = await http.post(
-//     Uri.parse('https://api.github.com/graphql'),
-//     headers: {
-//       'Authorization': 'Bearer $githubToken',
-//       'Content-Type': 'application/json',
-//     },
-//     body: jsonEncode({'query': query}),
-//   );
+    final categoryId = await _fetchGithubDiscussionCategoryId(
+      owner: owner,
+      repo: repo,
+      name: categoryName,
+    );
 
-//   final data = jsonDecode(response.body);
-//   final categories =
-//       data['data']['repository']['discussionCategories']['nodes'];
+    final query = '''
+    query {
+      repository(owner: "$owner", name: "$repo") {
+        discussions(first: 50, categoryId: "$categoryId") {
+          nodes {
+            title
+            url
+            answerChosenAt
+            body
+            answer {
+              body
+              createdAt
+            }
+          }
+        }
+      }
+    }
+  ''';
 
-//   for (var category in categories) {
-//     if (category['name'] == name) {
-//       return category['id'];
-//     }
-//   }
-//   return null;
-// }
+    final response = await http.post(
+      Uri.parse('https://api.github.com/graphql'),
+      headers: {
+        'Authorization': 'Bearer $githubToken',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({'query': query}),
+    );
+
+    final data = jsonDecode(response.body);
+    final discussions = data['data']['repository']['discussions']['nodes'];
+
+    for (var discussion in discussions) {
+      if (discussion['answerChosenAt'] != null) {
+        print('${discussion['title']}: ${discussion['url']}');
+      }
+    }
+  }
+
+  Future<String?> _fetchGithubDiscussionCategoryId({
+    required String owner,
+    required String repo,
+    required String name,
+  }) async {
+    final githubToken = Serverpod.instance.getPassword('githubToken')!;
+
+    final query = '''
+    query {
+      repository(owner: "$owner", name: "$repo") {
+        discussionCategories(first: 50) {
+          nodes {
+            id
+            name
+          }
+        }
+      }
+    }
+  ''';
+
+    final response = await http.post(
+      Uri.parse('https://api.github.com/graphql'),
+      headers: {
+        'Authorization': 'Bearer $githubToken',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({'query': query}),
+    );
+
+    final data = jsonDecode(response.body);
+    final categories =
+        data['data']['repository']['discussionCategories']['nodes'];
+
+    for (var category in categories) {
+      if (category['name'] == name) {
+        return category['id'];
+      }
+    }
+    return null;
+  }
+}
