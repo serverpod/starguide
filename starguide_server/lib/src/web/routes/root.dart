88af1b832f0624dafdcc91ca6abb7bd75d5f9d57
Viktor Lidholt <viktorlidholt@gmail.com>
2025-07-04 13:37:46 +0200

    feat: Adds Flutter web app.

--- starguide_server/lib/src/web/routes/root.dart
+++ starguide_server/lib/src/web/routes/root.dart
@@ -6,6 +6,20 @@ import 'package:serverpod/serverpod.dart';
 class RouteRoot extends WidgetRoute {
   @override
   Future<Widget> build(Session session, HttpRequest request) async {
+    // Disable cache.
+    request.response.headers.set(
+      'Cache-Control',
+      'no-store, no-cache, must-revalidate',
+    );
+    request.response.headers.set(
+      'Pragma',
+      'no-cache',
+    );
+    request.response.headers.set(
+      'Expires',
+      '0',
+    );
+
     return DefaultPageWidget();
   }
 }
--- starguide_server/lib/src/web/widgets/default_page_widget.dart
+++ starguide_server/lib/src/web/widgets/default_page_widget.dart
@@ -1,7 +1,7 @@
 import 'package:serverpod/serverpod.dart';
 
 class DefaultPageWidget extends Widget {
-  DefaultPageWidget() : super(name: 'default') {
+  DefaultPageWidget() : super(name: 'index') {
     values = {
       'served': DateTime.now(),
       'runmode': Serverpod.instance.runMode,
