0e67fa70070b4099f062541f6c12b1ea41c67014
Viktor Lidholt <viktorlidholt@gmail.com>
2025-07-17 11:57:22 +0200

    feat: Now executes searches in parallel.

---
+++ starguide_server/lib/src/business/search.dart
@@ -0,0 +1,80 @@
+import 'package:serverpod/serverpod.dart';
+import 'package:starguide_server/src/business/docs_table_of_contents.dart';
+import 'package:starguide_server/src/generated/protocol.dart';
+import 'package:starguide_server/src/generative_ai/generative_ai.dart';
+import 'package:starguide_server/src/generative_ai/prompts.dart';
+
+Future<List<RAGDocument>> searchDocumentation(
+  Session session,
+  List<ChatMessage> conversation,
+  String question,
+) async {
+  final genAi = GenerativeAi();
+  var documents = <RAGDocument>[];
+
+  // Search documentation for the most relevant URLs.
+  final toc = await DocsTableOfContents.getTableOfContents(session);
+  final urls = await genAi.generateUrlList(
+    Prompts.instance.get('search_toc_0')! +
+        toc +
+        Prompts.instance.get('search_toc_1')! +
+        question,
+  );
+
+  for (final url in urls) {
+    var document = await RAGDocument.db.findFirstRow(
+      session,
+      where: (t) => t.sourceUrl.equals(url),
+    );
+
+    if (document != null) {
+      documents.add(document);
+    }
+  }
+
+  return documents;
+}
+
+Future<List<RAGDocument>> searchDiscussions(
+  Session session,
+  List<ChatMessage> conversation,
+  String question,
+) async {
+  final genAi = GenerativeAi();
+
+  // Transform the question to a question to what it like looks like in the
+  // RAG database.
+  String transformedQuestion;
+  if (conversation.isEmpty) {
+    transformedQuestion = await genAi.generateSimpleAnswer(
+      Prompts.instance.get('transform_first_question')! + question,
+    );
+  } else {
+    final answerStream = genAi.generateConversationalAnswer(
+      systemPrompt: Prompts.instance.get('transform_followup_question')!,
+      question: question,
+      documents: [],
+      conversation: conversation,
+    );
+
+    // Concatenate the answer stream.
+    var answer = '';
+    await for (var chunk in answerStream) {
+      answer += chunk;
+    }
+    transformedQuestion = answer;
+  }
+
+  // Create an embedding for the question.
+  final embedding = await genAi.generateEmbedding(transformedQuestion);
+
+  // Find the most similar question in the RAG database.
+  final documents = await RAGDocument.db.find(
+    session,
+    orderBy: (rag) => rag.embedding.distanceCosine(embedding),
+    where: (t) => t.type.equals(RAGDocumentType.discussion),
+    limit: 5,
+  );
+
+  return documents;
+}
--- starguide_server/lib/src/endpoints/starguide_endpoint.dart
+++ starguide_server/lib/src/endpoints/starguide_endpoint.dart
@@ -1,5 +1,6 @@
 import 'package:serverpod/serverpod.dart';
 import 'package:starguide_server/src/business/docs_table_of_contents.dart';
+import 'package:starguide_server/src/business/search.dart';
 import 'package:starguide_server/src/generative_ai/generative_ai.dart';
 import 'package:starguide_server/src/util/random_string.dart';
 import 'package:starguide_server/src/generated/protocol.dart';
@@ -81,62 +82,12 @@ class StarguideEndpoint extends Endpoint {
 
     final genAi = GenerativeAi();
 
-    var documents = <RAGDocument>[];
-
-    // Search documentation for the most relevant URLs.
-    final toc = await DocsTableOfContents.getTableOfContents(session);
-    final urls = await genAi.generateUrlList(
-      Prompts.instance.get('search_toc_0')! +
-          toc +
-          Prompts.instance.get('search_toc_1')! +
-          question,
-    );
-
-    for (final url in urls) {
-      var document = await RAGDocument.db.findFirstRow(
-        session,
-        where: (t) => t.sourceUrl.equals(url),
-      );
-
-      if (document != null) {
-        documents.add(document);
-      }
-    }
-
-    String transformedQuestion;
-
-    // Transform the question to a question to what it like looks like in the
-    // RAG database.
-    if (conversation.isEmpty) {
-      transformedQuestion = await genAi.generateSimpleAnswer(
-        Prompts.instance.get('transform_first_question')! + question,
-      );
-    } else {
-      final answerStream = genAi.generateConversationalAnswer(
-        systemPrompt: Prompts.instance.get('transform_followup_question')!,
-        question: question,
-        documents: [],
-        conversation: conversation,
-      );
-
-      // Concatenate the answer stream.
-      var answer = '';
-      await for (var chunk in answerStream) {
-        answer += chunk;
-      }
-      transformedQuestion = answer;
-    }
-
-    // Create an embedding for the question.
-    final embedding = await genAi.generateEmbedding(transformedQuestion);
-
-    // Find the most similar question in the RAG database.
-    documents.addAll(await RAGDocument.db.find(
-      session,
-      orderBy: (rag) => rag.embedding.distanceCosine(embedding),
-      where: (t) => t.type.equals(RAGDocumentType.discussion),
-      limit: 5,
-    ));
+    // Run both searches in parallel.
+    final results = await Future.wait([
+      searchDocumentation(session, conversation, question),
+      searchDiscussions(session, conversation, question),
+    ]);
+    var documents = results.expand((list) => list).toList();
 
     // Generate the answer
     final answerStream = genAi.generateConversationalAnswer(
